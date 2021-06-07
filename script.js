4683c82e4916f2127ba906c88bfd24f9e05fe149
SIN88 <KG19882020@GMAIL.COM>
2021-06-07 12:05:48 -0700

    debugged and got styling to work

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+var containerel = document.querySelector("container");
+var apptitleel = document.querySelector("app-title");
+//var notificationEl = document.querySelector("");
 //var element = document.querySelector("");
 //var element = document.querySelector("");
 //var element = document.querySelector("");
 //var element = document.querySelector("");
 //var element = document.querySelector("");
-//var element = document.querySelector("");
-//var element = document.querySelector("");
-//var element = document.querySelector("");
+//var endpoint = `http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`;
+   // fetch(endpoint)
