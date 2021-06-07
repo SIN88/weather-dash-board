05fd98259507f3f1debf0d0f2ed181dc67d944c3
SIN88 <KG19882020@GMAIL.COM>
2021-06-07 12:34:19 -0700

    added api key

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-var containerel = document.querySelector("container");
-var apptitleel = document.querySelector("app-title");
-//var notificationEl = document.querySelector("");
-//var element = document.querySelector("");
-//var element = document.querySelector("");
-//var element = document.querySelector("");
-//var element = document.querySelector("");
-//var element = document.querySelector("");
-//var endpoint = `http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`;
-   // fetch(endpoint)
+var containerEl = document.querySelector("container");
+var appTitleEl = document.querySelector("app-title");
+var notificationsEl = document.querySelector("notifications");
+var weatherContainerEl = document.querySelector("weather-container");
+var temperatureEl = document.querySelector("temperature");
+var tempDescriptionEl = document.querySelector("temp-description");
+var locationEl = document.querySelector("location");
+//this api was taken from a previous activity just changed the api keywhich my key is 396bf9de82cd6c7d6db52ea80429e67c i added this after app id =
+var endpoint = `http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=396bf9de82cd6c7d6db52ea80429e67c&units=imperial`;
+    fetch(endpoint)
+
+    myElement = document.getElementById("submit")
+   
