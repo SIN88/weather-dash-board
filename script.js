6a3b310b914a27180afa9917e65cc30c485c9925
SIN88 <KG19882020@GMAIL.COM>
2021-06-08 18:17:16 -0700

    checked if api works

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ var weatherContainerEl = document.querySelector("weather-container");
 var temperatureEl = document.querySelector("temperature");
 var tempDescriptionEl = document.querySelector("temp-description");
 var locationEl = document.querySelector("location");
-//this api was taken from a previous activity just changed the api keywhich my key is 396bf9de82cd6c7d6db52ea80429e67c i added this after app id =
-var endpoint = `http://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=396bf9de82cd6c7d6db52ea80429e67c&units=imperial`;
-    fetch(endpoint)
 
+var api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=396bf9de82cd6c7d6db52ea80429e67c`;
+    fetch(api)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log('');
+        for (var i = 0; i < data.length; i++) {
+          console.log(data[i].name);
+        }
+      });
     myElement = document.getElementById("submit")
    
