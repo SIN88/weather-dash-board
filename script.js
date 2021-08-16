356887281143286260352513d23e3fe55c9dd0b1
SIN88 <KG19882020@GMAIL.COM>
2021-08-15 20:58:08 -0700

    cleaning up the code

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-var containerEl = document.querySelector("container");
-var appTitleEl = document.querySelector("app-title");
-var notificationsEl = document.querySelector("notifications");
-var weatherContainerEl = document.querySelector("weather-container");
-var temperatureEl = document.querySelector("temperature");
-var tempDescriptionEl = document.querySelector("temp-description");
-var locationEl = document.querySelector("location");
+const searchBtn= document.getElementById("searchBtn");
+const searchNow= document.getElementById("search");
+const api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=396bf9de82cd6c7d6db52ea80429e67c`;
+  
 
-var api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=396bf9de82cd6c7d6db52ea80429e67c`;
-    fetch(api)
-    .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log('');
-        for (var i = 0; i < data.length; i++) {
-          console.log(data[i].name);
-        }
-      });
-    myElement = document.getElementById("submit")
-   
+function getWeather() {
+  const cityName = searchNow.value
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${APIkey}`).then(function(res){return res.json()}).then(function(data){
+      console.log(data)
+      const title=document.getElementById("forecast-title");
+      title.textContent=data.name
+      addCityList(cityName)
+      const lat= data.coord.lat
+      const lon= data.coord.lon
+      fiveDayForeCast(lat,lon);
+  })
+}
